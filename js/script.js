61adb08d7035b01e18b1bb90092a36c97fc20cfb
maxberglund2 <tf2bro999@gmail.com>
2023-10-09 14:37:49 +0200

    html + js
    all code

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const container = document.getElementById("container");
+let taskArray = [];
+
+const createDivTask = (task) => {
+    const con = document.createElement("div");
+    const form = document.createElement("form");
+
+    container.appendChild(con);
+    con.appendChild(form);
+    form.setAttribute("action", "#");
+
+    const box = document.createElement("input");
+    box.setAttribute("type", "checkbox");
+    
+    const text = document.createElement("div");
+
+    const buttonsCon = document.createElement("div");
+    const del = document.createElement("input");
+    del.setAttribute("type", "button");
+    del.setAttribute("value", "Delete");
+    const edit = document.createElement("input");
+    edit.setAttribute("type", "button");
+    edit.setAttribute("value", "Edit");
+    buttonsCon.appendChild(del);
+    buttonsCon.appendChild(edit);
+
+    form.appendChild(box);
+    form.appendChild(text);
+    form.appendChild(buttonsCon);
+
+    text.textContent = task;
+
+    box.addEventListener("change", () => {
+        if (box.checked) {
+            text.style.textDecoration = "line-through";
+        } else {
+            text.style.textDecoration = "none";
+        }
+    });
+
+    if (box.checked) {
+        text.style.textDecoration = "line-through";
+    } else {
+        text.style.textDecoration = "none";
+    }
+
+    del.addEventListener("click", () => {
+        container.removeChild(con);
+    });
+
+    edit.addEventListener("click", () => {
+        text.textContent = document.getElementById("task").value;
+        document.getElementById("task").value = '';
+    });
+
+    //BOOTSTRAP
+    form.setAttribute("class", "d-flex justify-content-between align-items-center mt-2 border rounded-1 p-2");
+    del.setAttribute("class", "btn btn-danger mx-1");
+    edit.setAttribute("class", "btn btn-primary");
+};
+
+const createTask = () => {
+    let taskValue = document.getElementById("task").value;
+    taskArray.push(taskValue);
+    createDivTask(taskValue);
+
+    document.getElementById("task").value = '';
+
+    return false;
+};
